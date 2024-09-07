71122b54b9121ea9f505474f8a3cf044c14c830a
Vincent TWIZERE <116659480+vincenttwizere@users.noreply.github.com>
2024-09-07 21:05:55 +0300

    Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 1. Basic Annotations - String Concatenation
+"""
+
+def join_strings(first: str, second: str) -> str:
+    """ Joins two strings into one.
+    """
+    return first + second
